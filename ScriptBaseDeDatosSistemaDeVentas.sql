6e5ace4bc94f0cda70768ee2a4ed821a977b6054
Tristán Lacruz <lacruztristan@gmail.com>
2024-08-24 00:07:48 +0200

    Modificar Script Base de datos MySQL

---
+++ ScriptBaseDeDatosSistemaDeVentas.sql
@@ -0,0 +1,76 @@
+create database bd_sistema_ventas;
+
+use bd_sistema_ventas;
+
+-- crear tabla usuarios
+create table tb_usuario(
+idUsuario int (11) auto_increment primary key,
+nombre varchar(30) not null,
+apellido varchar(30) not null,
+usuario varchar(15) not null,
+password varchar(30) not null,
+telefono varchar(15) not null,
+estado int(1) not null
+);
+
+insert into tb_usuario (nombre, apellido, usuario, password, telefono, estado)
+values ("Tristan", "Lacruz", "tristan", "4321", "616258691", 1);
+
+select usuario, password from tb_usuario
+where usuario = "tristan" and password = "4321";
+
+-- crear tabla clientes
+create table tb_cliente(
+idCliente int (11) auto_increment primary key,
+nombre varchar(30) not null,
+apellido varchar(30) not null,
+cedula varchar(15) not null,
+telefono varchar(15) not null,
+direccion varchar(100) not null,
+estado int(1) not null
+);
+
+-- crear tabla categorías
+create table tb_categoria(
+idCategoria int (11) auto_increment primary key,
+descripcion varchar(200) not null,
+estado int(1) not null
+);
+
+-- crear tabla producto
+create table tb_producto(
+idProducto int (11) auto_increment primary key,
+nombre varchar(100) not null,
+cantidad int(11) not null,
+precio double(10,2) not null,
+descripcion varchar(200) not null,
+porcentajeIva int(2) not null,
+idCategoria int(11) not null,
+estado int(1) not null
+);
+
+-- crear tabla cabecera de venta
+create table tb_cabecera_venta(
+idCabeceraVenta int (11) auto_increment primary key,
+idCliente int(11) not null,
+valorPagar double(10,2) not null,
+fechaVenta date not null,
+estado int(1) not null
+);
+
+-- crear tablq detalle venta
+create table tb_detalle_venta(
+idDetalleVenta int (11) auto_increment primary key,
+idCabeceraVenta int(11) not null,
+idProducto int(11) not null,
+cantidad int(11) not null,
+precioUnitario double(10,2) not null,
+subtotal double(10,2) not null,
+descuento double(10,2) not null,
+iva double(10,2) not null,
+totalPagar double(10,2) not null,
+estado int(1) not null
+);
+
+
+    
